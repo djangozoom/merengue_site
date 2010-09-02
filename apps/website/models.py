320aad02e92a9722d081b2e76e01dbd3a0bb9a03
Manuel Saelices <msaelices@yaco.es>
2010-09-02 10:06:09 +0000

    Added primary and secondary links and a portal menu. Fixes #900.

--- apps/website/models.py
+++ apps/website/models.py
@@ -1,3 +1,52 @@
-#from django.db import models
+from south.signals import post_migrate
 
-# Create your models here.
+from merengue.section.models import Menu, ContentLink, AbsoluteLink
+
+
+def handle_post_migrate(sender, **kwargs):
+    if kwargs['app'] == 'website':
+        add_portal_menus()
+
+
+def add_portal_menus():
+    root_menu = Menu.objects.get(slug='portal_menu')
+    menu2 = Menu.objects.create(
+        name_en='Menu to Django',
+        slug='menu-django',
+        parent=root_menu,
+    )
+    link2 = AbsoluteLink.objects.create(
+        url='http://www.djangoproject.com/',
+        menu=menu2,
+    )
+    menu1 = Menu.objects.create(
+        name_en='Menu to test section',
+        slug='menu-test-section',
+        parent=root_menu,
+        lft=0,
+    )
+    link1 = AbsoluteLink.objects.create(
+        url='/test-section/',
+        menu=menu1,
+    )
+    menu1_2 = Menu.objects.create(
+        name_en='Homepage',
+        slug='menu-homepage',
+        parent=menu1,
+    )
+    link1_2 = ContentLink.objects.create(
+        content_id=1,
+        menu=menu1_2,
+    )
+    menu1_3 = Menu.objects.create(
+        name_en='Menu to doc1',
+        slug='menu-doc1',
+        parent=menu1,
+    )
+    link1_3 = ContentLink.objects.create(
+        content_id=4,
+        menu=menu1_3,
+    )
+
+
+post_migrate.connect(handle_post_migrate)
