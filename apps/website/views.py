80793eae3cbea0feed83d42baf26c7d3207358eb
Jorge Correa <jcorrea@yaco.es>
2011-02-11 12:33:31 +0000

    see #1467 fix website application after plugins refactoring

--- apps/website/views.py
+++ apps/website/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from plugins.core.config import PluginConfig as CoreConfig
 from merengue.base.models import BaseContent
+from merengue.pluggable.utils import get_plugin
 
 
 def index(request):
     """ Index page """
     # put here your staff
-    core_config = CoreConfig()
-    main_content_index = int(core_config.get_config()['home_initial_content'].get_value())
+    core_config = get_plugin('core').get_config()
+    main_content_index = int(core_config['home_initial_content'].get_value())
     content = BaseContent.objects.get(pk=main_content_index).get_real_instance()
     return render_to_response([content._meta.content_view_template,
                                'website/index.html'],
