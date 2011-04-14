a4fc95ec0249cc5cb873fbcdb79e299d3160cc6d
Manuel Saelices <msaelices@yaco.es>
2011-04-14 20:00:47 +0000

    Adapted this migration step. Fixes #1813. no-pep8. no-pyflakes.

--- apps/website/migrations/0002_install_demo_plugins.py
+++ apps/website/migrations/0002_install_demo_plugins.py
@@ -9,9 +9,13 @@ class Migration(DataMigration):
     def forwards(self, orm):
         "Write your forwards methods here."
         from django.conf import settings
-        from django.db import transaction
+        from johnny import cache
         from merengue.pluggable import register_plugin
         from merengue.pluggable.utils import install_plugin
+        # Unpatch cache backend to disable johnny cache. Johny does weird things
+        query_cache_backend = cache.get_backend()
+        query_cache_backend.unpatch()
+        from django.db import transaction
         for plug_dir in settings.DEMO_PLUGINS:
             plugin = register_plugin(plug_dir)
             plugin.installed = True
